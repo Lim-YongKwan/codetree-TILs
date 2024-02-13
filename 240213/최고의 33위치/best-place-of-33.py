583faceab28c18cff4c93993b5716edbb2b9cba8
Grizzly(Lim-YongKwan) <96565110+Lim-YongKwan@users.noreply.github.com>
2024-02-13 15:53:03 +0900

    Title: 최고의 33위치, Time: 126ms, Memory: 25MB, Status: Passed - CodeTree

--- 240213/최고의 33위치/best-place-of-33.py
+++ 240213/최고의 33위치/best-place-of-33.py
@@ -10,11 +10,13 @@ def get_num_of_gold(row_s, col_s, row_e, col_e):
 
     return num_of_gold
 
+max_gold = 0
+
 for row in range(n):
     for col in range(n):
         if row + 2 >= n or col + 2 >= n:
             continue
         num_of_gold = get_num_of_gold(row, col, row +2, col +2)
         max_gold = max(max_gold, num_of_gold)
-        
+
 print(max_gold)
