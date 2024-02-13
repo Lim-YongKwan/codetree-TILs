c200cc41cd7e31aed3e6fbc45415630b25466434
Grizzly(Lim-YongKwan) <96565110+Lim-YongKwan@users.noreply.github.com>
2024-02-13 23:10:59 +0900

    Title: 두 수의 합, Time: 43ms, Memory: 3MB, Status: Wrong Answer - CodeTree

---
+++ 240213/두 수의 합/sum-of-two-num.swift
@@ -0,0 +1,35 @@
+var input = readLine()!.split(separator: " ").map { Int(String($0))!}
+var arr = readLine()!.split(separator: " ").map{Int(String($0))!}
+
+arr.sort()
+var k = input[1]
+
+var dic = [Int: Int]()
+var exist = [Int: Int]()
+var answer = 0
+
+for i in stride(from: 0, to: arr.count, by: 1) {
+    var temp = arr[i]
+    dic[temp] = 0
+}
+
+
+for i in stride(from: 0, to: arr.count, by: 1) {
+    var temp = arr[i]
+    dic[temp]! += 1
+}
+
+for i in stride(from: 0, to: arr.count, by: 1) {
+    var temp = k - arr[i]
+    if(dic[temp] == 1 && dic[temp] != nil && exist[temp] == nil) {
+        answer += dic[temp]! * (dic[temp]! + 1) / 2
+    }
+    else if(dic[temp] != 0 && dic[temp] != nil && exist[temp] == nil) {
+        // print("answer : \(dic[temp])")
+        answer += dic[temp]! * (dic[temp]! - 1) / 2
+    }
+    exist[temp] = 1
+    exist[arr[i]] = 1
+}
+
+print(answer)
