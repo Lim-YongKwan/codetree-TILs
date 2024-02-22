a35a1197f01e316e6f4c67d40299d69567fb0456
Grizzly(Lim-YongKwan) <96565110+Lim-YongKwan@users.noreply.github.com>
2024-02-22 19:53:54 +0900

    Title: 최소공배수 구하기, Time: 142ms, Memory: 24MB, Status: Passed - CodeTree

--- 240222/최소공배수 구하기/find-the-least-common-multiple.py
+++ 240222/최소공배수 구하기/find-the-least-common-multiple.py
@@ -1,11 +1,10 @@
 n, m = map(int, input().split())
 
 def find_lcm(a, b):
-    for i in range(1, a*b):
+    for i in range(max(a,b), (a*b)+1):
         lcm = 0
         if i % a == 0 and i % b == 0:
             print(i)
             break
-
 
 find_lcm(n, m)
