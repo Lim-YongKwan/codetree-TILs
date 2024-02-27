88c00c622162208aa44f88e7fe75ef53eee3a3af
Grizzly(Lim-YongKwan) <96565110+Lim-YongKwan@users.noreply.github.com>
2024-02-27 16:09:23 +0900

    Title: 두 수의 거듭제곱, Time: 118ms, Memory: 24MB, Status: Passed - CodeTree

--- 240227/두 수의 거듭제곱/two-numbers-of-squares.py
+++ 240227/두 수의 거듭제곱/two-numbers-of-squares.py
@@ -1,5 +1,8 @@
 def find_answer(a, b):
-    return a^b
+    answer = 1
+    for i in range(1,b+1):
+        answer *= a
+    return answer
 
 a, b = map(int, input().split())
 print(find_answer(a,b))
