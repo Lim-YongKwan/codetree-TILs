e6d2a508724021b6795ce8cc63591ec2c1e3ba70
Grizzly(Lim-YongKwan) <96565110+Lim-YongKwan@users.noreply.github.com>
2024-03-01 21:05:15 +0900

    Title: 비율 구하기, Time: 19ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240301/비율 구하기/find-proportions.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <map>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    int T;
+    cin>>T;
+    map<string ,int> mp;
+    double total = 0;
+    for(int testcase = 0; testcase < T; testcase++) {
+        string s;
+        cin>>s;
+        mp[s] += 1;        
+        total++;
+    }
+    cout<<fixed;
+    cout.precision(4);
+
+    for(auto iter = mp.begin(); iter != mp.end(); iter++) {
+        cout<<iter->first<<" "<< (round)((double)iter->second / total * 100)<<endl;
+    }
+    
+    return 0;
+}
