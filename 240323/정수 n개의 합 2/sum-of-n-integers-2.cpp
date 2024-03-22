30d81e479d5684f22080d807aae199e5579d23d9
Grizzly(Lim-YongKwan) <96565110+Lim-YongKwan@users.noreply.github.com>
2024-03-23 00:25:41 +0900

    Title: 정수 n개의 합 2, Time: 35ms, Memory: 1MB, Status: Passed - CodeTree

--- 240323/정수 n개의 합 2/sum-of-n-integers-2.cpp
+++ 240323/정수 n개의 합 2/sum-of-n-integers-2.cpp
@@ -8,17 +8,17 @@ int main() {
     int n,m;
     cin>>n>>m;
     vector<int> num(n + 1, 0);
-    for(int i = 0;i<=n;i++){
+    for(int i = 1;i<=n;i++){
         cin>>num[i];
     }
     vector<int> pre_fix(n + 1 ,0);
-    for(int i = 0;i<=n;i++){
+    for(int i = 1;i<=n;i++){
         pre_fix[i] = num[i] + pre_fix[i - 1];
     }
 
     int answer = -100000;
     for(int i = m - 1;i<=n;i++){
-        answer = max(answer, pre_fix[i] - pre_fix[i - m + 1]);
+        answer = max(answer, pre_fix[i] - pre_fix[i - m]);
     }
     cout<<answer<<endl;
     return 0;
