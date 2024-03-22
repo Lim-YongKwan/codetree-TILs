f6334b5a7c096ab59a5672c2f3491bab3cce6189
Grizzly(Lim-YongKwan) <96565110+Lim-YongKwan@users.noreply.github.com>
2024-03-23 00:06:02 +0900

    Title: 정수 n개의 합 2, Time: 5090ms, Memory: 33MB, Status: Time Limit Exceed - CodeTree

--- 240323/정수 n개의 합 2/sum-of-n-integers-2.py
+++ 240323/정수 n개의 합 2/sum-of-n-integers-2.py
@@ -2,9 +2,7 @@ n, k = map(int, input().split())
 arr = [0] + list(map(int, input().split()))
 
 sum_A = [0] * (n+1)
-for i in range(5,0+k-1,-1):
-    #print(i)
+for i in range(n,0+k-1,-1):
     for j in range(k):
         sum_A[i] += arr[i-j]
-
 print(max(sum_A))
