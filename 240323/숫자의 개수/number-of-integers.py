1ac5eb110506e8da39a9c7dbb0cb9c2887b4b081
Grizzly(Lim-YongKwan) <96565110+Lim-YongKwan@users.noreply.github.com>
2024-03-23 16:33:36 +0900

    Title: 숫자의 개수, Time: 482ms, Memory: 33MB, Status: Passed - CodeTree

---
+++ 240323/숫자의 개수/number-of-integers.py
@@ -0,0 +1,31 @@
+n, m = map(int, input().split())
+arr = list(map(int, input().split()))
+arr2 = [0] * m
+
+def find_min_idx(target):
+    min_idx = n
+    left, right = 0, n-1
+    while left <= right:
+        mid = (left + right) // 2
+        if arr[mid] >= target:
+            min_idx = min(min_idx, mid)
+            right = mid-1
+        else:
+            left = mid+1
+    return min_idx
+
+def find_max_idx(target):
+    left, right = 0, n-1
+    min_idx = n
+    while left <= right:
+        mid = (left + right) // 2
+        if arr[mid] > target:
+            min_idx = min(min_idx, mid)
+            right = mid-1
+        else:
+            left = mid+1
+    return min_idx
+
+for i in range(m):
+    arr2[i] = int(input())
+    print(find_max_idx(arr2[i]) - find_min_idx(arr2[i]))
