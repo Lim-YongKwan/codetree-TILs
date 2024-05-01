9dc7e2c346bdcdd4a9680587c0bca1c7cdbb3b4e
Grizzly(Lim-YongKwan) <96565110+Lim-YongKwan@users.noreply.github.com>
2024-05-01 19:43:56 +0900

    Title: 오목, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240501/오목/O-mok.py
+++ 240501/오목/O-mok.py
@@ -6,8 +6,8 @@ white_win = 0
 for i in range(19):
     arr.append(list(map(int, input().split())))
 
-for i in range(15):
-    for j in range(15):
+for i in range(19):
+    for j in range(19):
         if arr[i][j] == 1:
             arr2.append([i, j+2, [arr[i][j], arr[i][j+1], arr[i][j+2], arr[i][j+3], arr[i][j+4]]])
             arr2.append([i+2, j, [arr[i][j], arr[i+1][j], arr[i+2][j], arr[i+3][j], arr[i+4][j]]])
@@ -20,7 +20,7 @@ for i in range(15):
             arr2.append([i+2, j+2, [arr[i][j], arr[i+1][j+1], arr[i+2][j+2], arr[i+3][j+3],arr[i+4][j+4]]])
             if i >=4 :
                 arr2.append([i-2, j+2, [arr[i][j], arr[i-1][j+1], arr[i-2][j+2], arr[i-3][j+3], arr[i-4][j+4]]])
-                
+
 for i in range(len(arr2)):
     if [1, 1, 1, 1, 1] in arr2[i]:
         black_win = 1
