2b9ae15e7f328cc0b2dcf94b0341d9fd1a86493c
Grizzly(Lim-YongKwan) <96565110+Lim-YongKwan@users.noreply.github.com>
2024-05-01 19:43:10 +0900

    Title: 오목, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240501/오목/O-mok.py
+++ 240501/오목/O-mok.py
@@ -18,7 +18,9 @@ for i in range(15):
             arr2.append([i, j+2, [arr[i][j], arr[i][j+1], arr[i][j+2], arr[i][j+3], arr[i][j+4]]])
             arr2.append([i+2, j, [arr[i][j], arr[i+1][j], arr[i+2][j], arr[i+3][j], arr[i+4][j]]])
             arr2.append([i+2, j+2, [arr[i][j], arr[i+1][j+1], arr[i+2][j+2], arr[i+3][j+3],arr[i+4][j+4]]])
-
+            if i >=4 :
+                arr2.append([i-2, j+2, [arr[i][j], arr[i-1][j+1], arr[i-2][j+2], arr[i-3][j+3], arr[i-4][j+4]]])
+                
 for i in range(len(arr2)):
     if [1, 1, 1, 1, 1] in arr2[i]:
         black_win = 1
