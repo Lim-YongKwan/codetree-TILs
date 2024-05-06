489349a65dfd7f5fa924f6707a57093bf5d48f49
Grizzly(Lim-YongKwan) <96565110+Lim-YongKwan@users.noreply.github.com>
2024-05-06 18:10:54 +0900

    Title: 최고의 13위치 2, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240506/최고의 13위치 2/best-place-of-13-2.py
+++ 240506/최고의 13위치 2/best-place-of-13-2.py
@@ -4,16 +4,23 @@ arr = [ list(map(int, input().split())) for _ in range(n)]
 max_sum = 0
 sum_val = 0
 second_max = 0
+sum_val2 = 0
+total_sum = 0
 
 for i in range(n):
     for j in range(n-2):
         sum_val = arr[i][j] + arr[i][j+1] + arr[i][j+2]
-        max_sum = max(max_sum, sum_val)
-
-for i in range(n):
-    for j in range(n-2):
-        sum_val = arr[i][j]+arr[i][j+1]+arr[i][j+2]
-        if sum_val != max_sum:
-            second_max = max(second_max, sum_val)
-
-print(max_sum+second_max)
+        for k in range(n):
+            for l in range(n-2):
+                if i == k:
+                    if l > j+2 and l < n-2:
+                        sum_val2 = arr[k][l] + arr[k][l+1]+arr[k][l+2]
+                    else:
+                        sum_val2 = 0
+                else:
+                    sum_val2 = arr[k][l] + arr[k][l+1] + arr[k][l+2]
+                second_max = max(second_max, sum_val2)
+        total_sum = sum_val + second_max
+        max_sum = max(max_sum, total_sum)
+        
+print(max_sum)
